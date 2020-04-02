65a03b1f8b5ea270f7554975c020f29e3105dde6
BeAce <beaceshimin@gmail.com>
2020-04-02 20:05:22 +0800

    feat: add babel scripts

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,21 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import less from 'rollup-plugin-less';
+import progress from "rollup-plugin-progress";
 import { terser } from "rollup-plugin-terser";
 
 import packageJSON from "./package.json";
 
-const input = "./src/components/index.js";
+const input = "src/index.js";
 // const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
 
 const plugins = [
+  progress(),
   babel({
     exclude: "node_modules/**"
   }),
   less({
-    output: './lib/host-ui.css'
+    output: 'lib/host-ui.css'
   }),
   external(),
   resolve(),
@@ -39,16 +41,17 @@ const config = files.map(file => {
     input,
     output: {
       file: file.file,
-      format: file.format
+      format: file.format,
     },
     plugins
   };
 
   if (file.format === 'umd') {
-    base.output.name = 'hostUI';
+    base.output.name = 'HostUI';
     base.output.globals = {
       react: "React",
-      'react-dom': "ReactDOM"
+      'react-dom': "ReactDOM",
+      classnames: 'classNames'
     }
   } else if (file.format === 'es') {
     base.output.exports = "named";
